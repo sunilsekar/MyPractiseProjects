06fbd149cee63787c64845ef9eda34d2e1289669
Your Name <you@example.com>
2020-01-02 22:31:09 +0530

    First commit

---
+++ BikeWorld/src/main/java/com/bike/BikeWorldApplication.java
@@ -0,0 +1,13 @@
+package com.bike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeWorldApplication.class, args);
+	}
+
+}
