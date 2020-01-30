35e5f6922cb0f96c0d6dedab507c7b92b8562018
Sekar <sekars@netapp.com>
2020-01-30 17:34:01 +0530

    login form input

--- AngularBikeWorld/src/app/app.module.ts
+++ AngularBikeWorld/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';  
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -23,6 +24,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(
       appRoutes
     )
