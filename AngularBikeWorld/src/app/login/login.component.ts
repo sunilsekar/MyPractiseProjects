8c15e2441c2be7e5bad606a62c46e947f548e0c8
Sekar <sekars@netapp.com>
2020-02-10 11:04:09 +0530

    Login http post subscribe changes

--- AngularBikeWorld/src/app/login/login.component.ts
+++ AngularBikeWorld/src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpService } from '../services/http.service';
-import { Response } from '../models/response.model';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +13,6 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router, private httpService: HttpService) { }
 
   loginFormGroup: FormGroup;
-  loginResponse: Response;
 
   ngOnInit() {
     this.loginFormGroup = new FormGroup({
@@ -24,18 +22,14 @@ export class LoginComponent implements OnInit {
   }
 
   onLogin() {
-    this.httpService.postLogin(this.loginFormGroup.value).subscribe(response => this.loginResponse = {
-      status: response.status,
-      message: response.message,
-      token: response.token
+    this.httpService.postLogin(this.loginFormGroup.value).subscribe(response => { 
+      if (response.status) {
+        console.log("Login successful!");
+        this.router.navigate(["/home"]);
+      } else {
+        console.log("Login failed!");
+      }
     });
-    //console.log("onLogin :: " + this.loginResponse.message);
-    if (this.loginResponse.status) {
-      console.log("Login successful!");
-      this.router.navigate(["/home"]);
-    } else {
-      console.log("Login failed!");
-    }
   }
 
 }
